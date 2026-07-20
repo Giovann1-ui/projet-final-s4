1a3dbc70a0743a39e6249fe18984bba3c3feab1d
Giovanni Rambatoson <rggiovanni16@gmail.com>
2026-07-20 11:20:05 +0300

    [feat]: inseretion d'operateur

---
+++ app/Models/PrefixeOperateurModel.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PrefixeOperateurModel extends Model
+{
+    protected $table = 'prefixe_operateur';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['code'];
+
+    public function getAllPrefixeOperateurs()
+    {
+        return $this->findAll();
+    }
+
+    public function getPrefixeOperateurById($id = null)
+    {
+        return $this->find($id);
+    }
+
+    public function createPrefixeOperateur($data = [])
+    {
+        return $this->insert($data);
+    }
+}
